fa2bdfdf774b77a6ddf7647a517d537982cb30a1
Alex Leitch <alex.leitch@gmail.com>
2019-02-04 15:06:55 -0500

    Fishes out audio information well enough

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 
+const dir = './imports';
+const VALID_SOUND_EXTENSIONS = /.mp3/gi;
+
 // thanks to https://gist.github.com/kethinov/6658166#gistcomment-2774154
 // [].flat only works in very recent browsers and servers: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 const walkSync = (dir, filelist = []) => {
@@ -18,36 +21,39 @@ const walkSync = (dir, filelist = []) => {
       filelist.push(dirFile);
     }
   }
-  filelist = filelist.flat();
-  return filelist;
+  return filelist.flat();
 };
 
-let files = walkSync('./imports');
+return walkSync(dir)
+  .filter((f) => { // Filter out non-edl files
+    const strArray = f.split("\.");
+    return strArray[strArray.length - 1] === 'edl';
+  })
+  .map((m) => { // Read each file so we can start messing with the numbers
+    fs.readFile(m, 'utf8', function (err, data) {
+      if (err) throw err;
 
-// Filter out non-edl files
-files = files.filter((f) => {
-  const strArray = f.split("\.");
-  return strArray[strArray.length - 1] === 'edl';
-})
+      return data.split(/\r\n|\r|\n/) // split lines up
+        .reduce((col, item, i) => {
+          const hit = Boolean(item.match(VALID_SOUND_EXTENSIONS));
+          if (hit) {
+            let columns = splitFileByLines[i - 1];
+            const index = item.lastIndexOf(" ") + 1;
+            const fileName = item.substr(index);
 
-// Read each file so we can start messing with the numbers
-files.map((m) => {
-  fs.readFile(m, 'utf8', function (err, data) {
-    if (err) throw err;
-
-    const splitFileByLines = data.split(/\r\n|\r|\n/);
-
-    const columnsAndFileNames = splitFileByLines.reduce((col, item, i) => {
-      const hit = Boolean(item.match(/.mp3/gi));
-      if (hit) {
-        col.push({
-          columns: splitFileByLines[i - 1],
-          file: splitFileByLines[i]
-        });
-      }
-      return col;
-    }, []);
-
-    console.log(columnsAndFileNames);
-  });
-})
+            const startColumns = columns.indexOf(":") - 2;
+            columns = columns.substr(startColumns).split(" ");
+            col.push({
+              columns: {
+                sourceIn: columns[0],
+                sourceOut: columns[1],
+                recordIn: columns[2],
+                recordOut: columns[3]
+              },
+              file: fileName
+            });
+          }
+          return col;
+        }, []);
+    });
+  })
