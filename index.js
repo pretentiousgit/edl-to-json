e67500e6699a716a199dd01810608952389d78f6
Alex Leitch <alex.leitch@gmail.com>
2019-02-04 16:55:44 -0500

    Add moment formatting for when lines appear in video

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const exportFile = 'edlJson';
 const dir = './imports';
 const VALID_SOUND_EXTENSIONS = /.mp3/gi;
 
+const moment = require('moment');
+const momentFormat = 'HH:mm:ss.SS'
+
+
+function replaceAt(target, index, replacement) {
+  return target.substr(0, index) + replacement + target.substr(index + replacement.length);
+}
 // thanks to https://gist.github.com/kethinov/6658166#gistcomment-2774154
 // [].flat only works in very recent browsers and servers: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 const walkSync = (dir, filelist = []) => {
@@ -14,7 +21,6 @@ const walkSync = (dir, filelist = []) => {
     const dirFile = path.join(dir, file);
     const dirent = fs.statSync(dirFile);
     if (dirent.isDirectory()) {
-      console.log('directory', path.join(dir, file));
       var odir = [];
       odir = walkSync(dirFile, dir.files);
       filelist.push(odir);
@@ -25,10 +31,9 @@ const walkSync = (dir, filelist = []) => {
   return filelist.flat();
 };
 
-
 const EDLToJsonReducer = (lineArray = []) => {
   return lineArray.reduce((col, item, i) => {
-    const hit = Boolean(item.match(/.mp3/gi));
+    const hit = Boolean(item.match(VALID_SOUND_EXTENSIONS));
     if (hit) {
       let columns = lineArray[i - 1];
       const index = item.lastIndexOf(" ") + 1;
@@ -36,6 +41,11 @@ const EDLToJsonReducer = (lineArray = []) => {
 
       const startColumns = columns.indexOf(":") - 2;
       columns = columns.substr(startColumns).split(" ");
+      columns = columns.map((m) => {
+        const lastIndex = m.lastIndexOf(":");
+        m = replaceAt(m, lastIndex, '.');
+        return m;
+      })
       col.push({
         columns: {
           sourceIn: columns[0],
@@ -58,7 +68,6 @@ files = files.filter((f) => {
   return strArray[strArray.length - 1] === 'edl';
 })
 
-
 const fileArray = files.map((m) => fs.readFileSync(m, 'utf8'));
 
 const output = fileArray.reduce((col, item, i) => {
@@ -68,14 +77,42 @@ const output = fileArray.reduce((col, item, i) => {
   return col
 }, { data: {} });
 
-console.log(output);
+function returnDuration(srcOut, srcIn) {
+  const dur = moment.duration(srcOut.diff(srcIn));
+  const seconds = dur.seconds();
+  const milliseconds = dur.milliseconds();
+  return seconds + milliseconds / 1000;
+}
+// Now let's find the duration and when to turn things on and off
+const data = Object.keys(output.data).map((m) => {
+  return output.data[m].map((n) => {
+    console.log('CHECK N', n);
+    const sourceOut = moment(n.columns.sourceOut, "HH:mm:ss.SS");
+    const sourceIn = moment(n.columns.sourceIn, "HH:mm:ss.SS");
+
+    const recOut = moment(n.columns.recordOut, "HH:mm:ss.SS");
+    const recIn = moment(n.columns.recordIn, "HH:mm:ss.SS");
+
+    n.duration = returnDuration(sourceOut, sourceIn);
+    n.videoDuration = returnDuration(recOut, recIn);
+
+    const lengthMatch = Boolean(n.duration == n.videoDuration);
+    // if we consistently get non-matching video lengths,
+    // we should figure out how to adjust them next to the tape automatically - closer or further out
+
+    n.videoInject = Number(recIn.format("s.SS"));
+    return n;
+  })
+})
+
+console.log(data);
 
 // make the directory if it has been deleted
 if (!fs.existsSync(exportDir)) {
   fs.mkdirSync(exportDir);
 }
 
-fs.writeFile(`${exportDir}/${exportFile}`, JSON.stringify(output, null, '\t'), (err) => {
+fs.writeFile(`${exportDir}/${exportFile}`, JSON.stringify(data, null, '\t'), (err) => {
   if (err) throw err;
   console.log('Saved EDL to JSON file');
 });
